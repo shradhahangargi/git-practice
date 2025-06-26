586804fd78393468ce3ae5def606424f85ffb8c3
Shradha Hangargi <shradhahangargi@Shradhas-Air.cust.communityfibre.co.uk>
2025-06-26 16:19:52 +0100

    Add AddNumbers.java with sum feature

---
+++ AddNumbers.java
@@ -0,0 +1,14 @@
+public class AddNumbers {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 30;
+
+        int sum = a + b;
+        int product = a * b;
+        int difference = a - b;
+
+        System.out.println("Sum: " + sum);
+        System.out.println("Product: " + product);
+        System.out.println("Difference: " + difference);
+    }
+}
